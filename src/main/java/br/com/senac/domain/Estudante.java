cb3d953df754d126899955543d222ebb8026238e
Yuri Macedo <yuri_paulino@hotmail.com>
2023-03-20 22:12:38 -0300

    first commit

---
+++ src/main/java/br/com/senac/domain/Estudante.java
@@ -0,0 +1,59 @@
+package br.com.senac.domain;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Estudante {
+
+	private Long id;
+	
+
+	private String emailString;
+	
+
+	private LocalDate dataNascimento;
+	
+	private String nome;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmailString() {
+		return emailString;
+	}
+
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
